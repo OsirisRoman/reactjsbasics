ff399785f5c4a33049de30b2edfb7db63615669d
Osiris Román <osirisr1994@gmail.com>
2020-11-06 21:51:08 -0500

    Primeros pasos con Hooks(setState)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
 function App() {
+  //Crear listado de productos
+  const [productos, guardarProductos] = useState([
+    { id: 2, nombre: 'Camisa Mongo', precio: 50 },
+    { id: 3, nombre: 'Camisa Express', precio: 60 },
+    { id: 1, nombre: 'Camisa ReactJS', precio: 70 },
+    { id: 4, nombre: 'Camisa NodeJS', precio: 80 },
+  ]);
+
+  //Obtener fecha
   const fecha = new Date().getFullYear();
 
   return (
