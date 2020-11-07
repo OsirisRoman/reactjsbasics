eb4a313485457c68d100f261dd218630f18fd436
Osiris Román <osirisr1994@gmail.com>
2020-11-06 22:36:14 -0500

    Manejando Eventos onClick
    Se crearon 2 tipos de funciones para 2 botones distintos, y se las probó
    para conocer mejor el paso de funciones en eventos onClick

--- src/components/Producto/Producto.jsx
+++ src/components/Producto/Producto.jsx
@@ -5,9 +5,33 @@ import React from 'react';
 const Producto = ({ producto }) => {
   const { nombre, precio } = producto;
 
+  //Agregar al carrito
+  const seleccionarProducto = (nombreProducto) => {
+    console.log(`Comprando ${nombreProducto}...`);
+  };
+
+  //Comprando
+  const comprando = () => {
+    console.log('comprando');
+  };
+
   return (
     <p>
-      {nombre}: ${precio}
+      {nombre}: ${precio}{' '}
+      <button
+        type='button'
+        id='comprar'
+        //notar que esta función con parámetros debe pasar dentro de un anonymous function
+        onClick={() => seleccionarProducto(nombre)}>
+        Comprar Este Producto
+      </button>
+      <button
+        type='button'
+        id='comprando'
+        //esta función SIN parámetros debe pasar sin paréntesis
+        onClick={comprando}>
+        comprando
+      </button>
     </p>
   );
 };
